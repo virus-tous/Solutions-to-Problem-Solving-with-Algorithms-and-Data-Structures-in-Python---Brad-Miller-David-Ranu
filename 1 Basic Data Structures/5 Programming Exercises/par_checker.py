631572cec398ce260008747582458ee1894f1e2f
Virustous <just.habit.and.love@gmail.com>
2021-10-21 20:28:38 +0300

    programming exercises

---
+++ 1 Basic Data Structures/5 Programming Exercises/par_checker.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 12 14:52:59 2021
+
+@author: virustous
+"""
+
+import Stack
+
+def par_checker(string):
+    '''check if a string's parentheses appear in a balanced fashion'''
+    s = Stack.Stack()
+    
+    for ch in string:
+        if ch == '(':
+            s.push(ch)
+        else:
+            if s.isEmpty():
+                return False
+            else:
+                s.pop()
+    return s.isEmpty()
+# end f. par_checker
+
+# print(par_checker('((()))'))
+# print(par_checker('(()'))
